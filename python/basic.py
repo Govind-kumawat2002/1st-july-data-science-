1243814f2ddb7255daa6229f682de25b3983bd18
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-07-11 17:27:26 +0530

    numpy and git github

---
+++ python/basic.py
@@ -0,0 +1,236 @@
+# print("hello hii kese ho ")
+# a = "hello"
+# num = 10
+# @ = 4
+# 2= 2
+# Age = 22
+# age = 23 
+# print(Age)
+# print(age)
+# break = 22
+# Print = 22
+# print(Print)
+
+# num 
+# char 
+# double 
+# float
+# int 
+# count 
+# str 
+# long 
+# break
+# list 
+# tuple
+# bool
+# set 
+# dict 
+# complex
+# range
+
+# string 
+
+# print(name)
+# print("my name is ",name)
+# print("name dtypes:-",type(name))
+# print("len of name:- ",len(name))
+
+### indexing 
+# print(name[0])
+# print(name[2])
+# print(name[len(name)-1])
+
+## slicing in python 
+
+# print(name[0:4])
+
+# howe work 
+
+### operation 
+# name = "rirtik"
+# last = "KUMAWAT"
+# upper_case =name.upper() ### upper function ka used upper case ke liye krte hai 
+
+# print(upper_case)
+# lower_last_name=last.lower()
+# print(lower_last_name)
+
+# print(name.count("r"))
+# name = "ritik"
+# last = " KUMAWAT"
+# print(name.title())
+# print(name.capitalize())
+# print(name+last)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>.list >>>>>>>>>>>>>.
+# list = 
+# array or list >>>>>>>>> 
+# mutable 
+# duplicated values 
+# ## herto
+# order 
+# array  >>>>>   lst = [1,2,3,4,5,6] 
+# array
+# print("my first list:-",lst) 
+# print("len of list:-",len(lst))
+# print("type of list:-",type(lst))
+# # list=[1,2,3,4,5]
+# # print(list) 
+# print(lst[0])
+# print(lst[5])
+
+# print(lst[0:5])
+# lst = [1,2,2,3,4,5,6,7, 2.3,] #### >>>>>>>>list
+# lst.pop()
+# print(lst)
+# lst.append(100)
+# print(lst)
+# lst.insert(0,1000)
+# print(lst)
+# copy_lst =lst.copy()
+# print(copy_lst)
+# lst.reverse()
+# lst.remove(2.3)
+# print(lst)
+# lst.sort()
+# lst = [1,2,2,3,4,5,6,7, 2.3,] #### >>>>>>>>list
+
+# print(lst.count(2))
+# print(lst)
+
+
+# >>>>>>>>>>>>>>>>>>>>tuple <<<<<<<<<<<<<<<
+# tuple = 
+# tpl = (1,2,3,5,585,5, "ritik", 2.3)
+# print("my first tuple :-",tpl)
+# print("len of tuple:-",len(tpl))
+# print("type of tuple :- ",type(tpl))
+
+# print(tpl[0])
+# print(tpl[2])
+# print(tpl[0:6])
+# build
+# a = 1,2,58,5,58,5,5,85,2
+# # print(10)
+# print(a) # >>>>(1,2,58,5,58,5,5,85,2)
+# print(type(a))  ## <class 'tuple'>
+# print(len(a)) ## 9
+
+# a , b ,c =(1,2,3)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# a , b ,c  = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+
+
+# tpl = (1,2,58,2,8,2,8)
+# print("max of tuple",max(tpl))
+# print("min of tuple",min(tpl))
+# print("sum of tuple",sum(tpl))
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>dict >>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+# my_dict = {
+#     "name":"Rohit", #### name , class , Roll number and Address >>>>keys 
+#     "class": "2nd year",  #### rohit , 2nd year , 21EAYCS055 and jaipur >>values 
+#     "Roll number":"21EAYCS055",
+#     "Address":"jaipur","name":"Rohit"   #### >>>"name":"Rohit" item 
+# }
+
+# print("my first dict :- ",my_dict)
+# print("len of dict :-",len(my_dict))
+# print("type of dict :- ",type(my_dict))
+# print(my_dict['name'])
+# print(my_dict['class'])
+# print(my_dict['Roll number'])
+# print(my_dict['Address'])
+# my_dict['Address']="new jaipur "
+# print(my_dict)
+# my_dict = {
+#     "name":"Rohit", #### name , class , Roll number and Address >>>>keys 
+#     "class": "2nd year",  #### rohit , 2nd year , 21EAYCS055 and jaipur >>values 
+#     "Roll number":"21EAYCS055",
+#     "Address":"jaipur","name":"Rohit"   #### >>>"name":"Rohit" item 
+# }
+# my_dict['branch']="Cse"
+# print(my_dict)
+
+
+# tast1 >>>>>> update function
+#  
+# get function  >>>> 5 or []
+
+
+# [1,2,2,[1,2,5]4,1,]
+
+
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.items())
+
+
+# # copy_dict =my_dict.copy()
+# # print(copy_dict)
+
+# my_dict.clear()
+# print(my_dict)
+
+# a =input("enter a number ")
+# b =input("enter second number")
+# print(a*b)
+# print(type(a))
+# print(type(a))
+
+# Type casting 
+# a =int(input("enter a number "))
+# b =int(input("enter second number"))
+# print(a*b)
+
+# lst = [1,2,6,8,5,58]
+# print("this is my list:-",lst)
+# print("type of list :-",type(lst))
+# tpl  =tuple(lst)
+# print("this is my tuple:-",tpl)
+# print("type of tuple:-",type(tpl))
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>set >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# my_set = 
+# tuple = 
+# list 
+# set 
+# dict 
+
+# my_set = {1,2,3,5,41,6,1,5, "jai"}### >>>>>>>>>>>>> 
+# print("this is my first set :-",my_set)
+# print("len of my set:-",len(my_set))
+# print("type of set :-",type(my_set))
+
+# task >>>>
+# my_set.union()
+# my_set.intersection()
+# my_set.difference()
+# lst =list(my_set)
+
+### operatore 
+
+# lst.append(1000)
+# print(set(lst))
+
+
+
+
+
+
+
+
+
